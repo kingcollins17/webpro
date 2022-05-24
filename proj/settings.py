0f746b7aa4e09522c5ddad180777c31faa51a543
kingcollins17 <98811110+kingcollins17@users.noreply.github.com>
2022-05-24 18:37:19 +0100

    modified allowed hosts

--- proj/settings.py
+++ proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b50e!!^_$d+8_7=mv^#b*!%=gc2@p@=#53hdhq2cls4#smwaty
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tashub.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
